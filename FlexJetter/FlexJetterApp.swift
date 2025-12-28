5e0229b0f14d2b4ed071f6ef131c2070b9397e5e
Jonathan <jonny.k242@gmail.com>
2025-12-28 11:31:44 -0500

    Initial Commit

---
+++ FlexJetter/FlexJetterApp.swift
@@ -0,0 +1,32 @@
+//
+//  FlexJetterApp.swift
+//  FlexJetter
+//
+//  Created by Jonathan on 12/28/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct FlexJetterApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
